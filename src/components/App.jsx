d8aa07c7046b7e299143c222537707c2ce902fb6
Daniil <negativdaniil4@gmail.com>
2024-03-08 23:05:41 +0200

    ReactHomework1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
 export const App = () => {
+
+  const name = "Daniil";
+  const google = [
+    "Google: ",
+    "https://www.google.com/",
+  ];
+  const num1 = 6*7;
+  const num2 = 45+23;
+  const colors = [
+    "Червоний",
+    "Синій",
+    "Зелений"
+  ]
+  const colorMap = colors.map(color => {
+    return <li>{color}</li>
+  });
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>{name}</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <a href="">{google}</a>
+      <p>{num1}<br/>{num2}</p>
+      <ul>
+        {colorMap}
+      </ul>
+      <img src="https://static-00.iconduck.com/assets.00/react-icon-2048x2048-o8k3ymqa.png"  alt="react" />
     </div>
   );
 };
